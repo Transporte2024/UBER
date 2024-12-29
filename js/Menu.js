639cd521ca177164d77739d3e8686ab94c46dab7
Joselyn13000 <fatimagarcia49651@gmail.com>
2024-12-28 22:31:56 -0600

    Launche 1.1

---
+++ js/Menu.js
@@ -0,0 +1,6 @@
+const menus = document.querySelector('.menu')
+const menu = document.querySelector('.menu-navegacion')
+
+menus.addEventListener('click', ()=>{
+    menu.classList.toggle("spread")
+})
